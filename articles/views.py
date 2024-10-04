d6dac98a5ff91a190157bcca1cf5eee3cc2d297e
Ronnel Ruanes <rruanes@gmail.com>
2024-10-04 17:27:33 +0800

    permissions and authorizations

--- articles/views.py
+++ articles/views.py
@@ -1,7 +1,9 @@
 # articles/views.py
-from django.views.generic import ListView, DetailView, CreateView
-from django.views.generic.edit import UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import UpdateView, DeleteView, CreateView
 from django.urls import reverse_lazy
+
 from .models import Article
 
 class ArticleListView(ListView):
@@ -12,24 +14,29 @@ class ArticleDetailView(DetailView):
     model = Article
     template_name = "article_detail.html"
 
-class ArticleUpdateView(UpdateView):
+class ArticleUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Article
-    fields = (
-        "title",
-        "body",
-    )
+    fields = ("title", "body")
     template_name = "article_edit.html"
+    
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
 
-class ArticleDeleteView(DeleteView):
+class ArticleDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Article
     template_name = "article_delete.html"
     success_url = reverse_lazy("article_list")
 
-class ArticleCreateView(CreateView):
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
+
+class ArticleCreateView(LoginRequiredMixin, CreateView):
     model = Article
     template_name = "article_new.html"
-    fields = (
-        "title",
-        "body",
-        "author",
-    )
+    fields = ("title", "body",)
+    
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
